cd7eae09d85f916f012978d9cc37772536c40f17
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-04 22:23:56 +0700

    Update Client.jsx

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -14,9 +14,9 @@ import clientImgLogo12 from "../assets/image/img-logo1.png";
 import clientImgLogo13 from "../assets/image/img-logo2.png";
 import clientImgLogo14 from "../assets/image/img-logo3.png";
 import HomeCarouselBrand from "./carousel/HomeCarouselBrand";
+import BrandSlider from "./BrandSlider";
 
 const clientsTmgLogo = [
-  
   //   clientImgLogo01,
   //   clientImgLogo02,
   //   clientImgLogo03,
@@ -48,19 +48,11 @@ function Client() {
                 <img src={client} alt="" />
             </li>
         ))} */}
-        <HomeCarouselBrand />
-        {/* <BrandSlider /> */}
+        {/* <HomeCarouselBrand /> */}
+        <BrandSlider/>
       </ul>
     </div>
   );
 }
 
 export default Client;
-
-
-
-
-
-
-
-
