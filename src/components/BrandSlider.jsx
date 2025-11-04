3bf433780090136d4ed026c295dcec374c50d67c
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-04 22:23:32 +0700

    Update BrandSlider.jsx

--- src/components/BrandSlider.jsx
+++ src/components/BrandSlider.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 
 // import รูปจาก assets
-import accretech from "../assets/images/logo/accretech.png";
-import accud from "../assets/images/logo/accud.png";
-import aikoh from "../assets/images/logo/aikoh.png";
-import carmar from "../assets/images/logo/carmar.png";
-import carton from "../assets/images/logo/carton.png";
-import chuer from "../assets/images/logo/chuer.png";
-import digicon from "../assets/images/logo/digicon.png";
-import eisen from "../assets/images/logo/eisen.png";
-import fluk from "../assets/images/logo/fluk.png";
-import gin from "../assets/images/logo/gin.png";
-import hexagon from "../assets/images/logo/hexgon.png";
-import imada from "../assets/images/logo/imada.png";
-import insize from "../assets/images/logo/insize.png";
-import jadever from "../assets/images/logo/jadever.png";
-import mahr from "../assets/images/logo/mahr.png";
-import mitutoyo from "../assets/images/logo/mitutoyo.png";
-import ojiyas from "../assets/images/logo/ojiyas.png";
-import peacock from "../assets/images/logo/peacock.png";
-import shahe from "../assets/images/logo/shahe.png";
-import shimadzu from "../assets/images/logo/shimadzu.png";
-import zepper from "../assets/images/logo/zepper.png";
-import tboss from "../assets/images/logo/t-boss.png";
-import tanita from "../assets/images/logo/tanita.png";
-import vertex from "../assets/images/logo/vertex.png";
+import accretech from "../assets/image/logo/accretech.png";
+import accud from "../assets/image/logo/accud.png";
+import aikoh from "../assets/image/logo/aikoh.png";
+import carmar from "../assets/image/logo/carmar.png";
+import carton from "../assets/image/logo/carton.png";
+import chuer from "../assets/image/logo/chuer.png";
+import digicon from "../assets/image/logo/digicon.png";
+import eisen from "../assets/image/logo/eisen.png";
+import fluk from "../assets/image/logo/fluk.png";
+import gin from "../assets/image/logo/gin.png";
+import hexagon from "../assets/image/logo/hexagon.png";
+import imada from "../assets/image/logo/imada.png";
+import insize from "../assets/image/logo/insize.png";
+import jadever from "../assets/image/logo/jadever.png";
+import mahr from "../assets/image/logo/mahr.png";
+import mitutoyo from "../assets/image/logo/mitutoyo.png";
+import ojiyas from "../assets/image/logo/ojiyas.png";
+import peacock from "../assets/image/logo/peacock.png";
+import shahe from "../assets/image/logo/shahe.png";
+import shimadzu from "../assets/image/logo/shimadzu.png";
+import zepper from "../assets/image/logo/zepper.png";
+import tboss from "../assets/image/logo/t-boss.png";
+import tanita from "../assets/image/logo/tanita.png";
+import vertex from "../assets/image/logo/vertex.png";
+import morewright from "../assets/image/logo/more&wright.png";
 
 const brands = [
   accretech,
@@ -46,14 +47,15 @@ const brands = [
   ojiyas,
   peacock,
   shahe,
-  shimada,
+  shimadzu,
   zepper,
   tboss,
   tanita,
-  vertex
+  vertex,
+  morewright,
 ];
 
-export default function BrandSlider(){
+export default function BrandSlider() {
   return (
     <div className="overflow-hidden py-8 bg-white">
       <div className="relative flex w-max animate-scroll">
@@ -64,12 +66,12 @@ export default function BrandSlider(){
           >
             <img
               src={logo}
-              alt={`Brand ${index}`}
+              alt={`Brand ${index + 1}`}
               className="h-12 w-auto object-contain grayscale hover:grayscale-0 transition-all duration-300"
             />
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
