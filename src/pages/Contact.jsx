d43900998b3b24ea7d2e46eee975903f59889c97
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-11 14:29:34 +0700

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -58,7 +58,7 @@ export default function ContactUs() {
             <h2 className="text-3xl md:text-4xl font-extrabold text-blue-900 uppercase drop-shadow-sm">
             GOAL FLOW INSTRUMENT CO., LTD.
           </h2>
-          <p className="text-gray-600 text-lg mt-2">
+          <p className="text-gray-600 text-lg mt-2 p-10">
             บริษัท โกลโฟล อินตรูเมนท์ จำกัด
           </p>
           </motion.dev>
@@ -207,4 +207,5 @@ export default function ContactUs() {
      </section>
   );
 }
+
 
