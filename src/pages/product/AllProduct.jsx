c1a28ebf333403d32dcc03014db5484c4dd2fac6
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-12 00:28:10 +0700

    Update AllProduct.jsx

--- src/pages/product/AllProduct.jsx
+++ src/pages/product/AllProduct.jsx
@@ -107,7 +107,7 @@ const handleCheck = (e) => {
             <div>
               <MenuButton className="inline-flex items-center gap-x-2 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow hover:bg-gray-100 ring-1 ring-gray-300 transition-all duration-200">
                 {/* <SearchCardAllProduct /> */}
-                {/* <ChevronDownIcon className="w-5 h-5 text-gray-500" /> */}
+                <ChevronDownIcon className="w-5 h-5 text-gray-500" />
               </MenuButton>
             </div>
 
@@ -117,7 +117,7 @@ const handleCheck = (e) => {
 
                   {({ active }) => (
                     <button
-                      onClick={() => setCategorySelected("ทั้งหมด")}
+                      onClick={() => setSelectedCategory("ทั้งหมด")}
                       className={`${
                         active ? "bg-blue-50 text-blue-600" : "text-gray-700"
                       } block w-full text-left px-4 py-2 text-sm`}
@@ -131,7 +131,7 @@ const handleCheck = (e) => {
                     <MenuItem key={cat.id || cat._id}>
                       {({ active }) => (
                         <button
-                          onClick={() => setCategorySelected(cat.name)}
+                          onClick={() => setSelectedCategory(cat.name)}
                           className={`${
                             active
                               ? "bg-blue-50 text-blue-600"
@@ -201,6 +201,7 @@ const handleCheck = (e) => {
 };
 
 export default AllProduct;
+
 
 
 
