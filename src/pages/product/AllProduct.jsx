27861981b9fd6f7d131d38a9517c1d96d94737a8
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-11 23:51:18 +0700

    Update AllProduct.jsx

--- src/pages/product/AllProduct.jsx
+++ src/pages/product/AllProduct.jsx
@@ -85,10 +85,10 @@ const AllProduct = () => {
           <Menu as="div" className="relative inline-block text-left">
             <div>
               <MenuButton className="inline-flex items-center gap-x-2 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow hover:bg-gray-100 ring-1 ring-gray-300 transition-all duration-200">
-                
-                {products.map((item,index)=>(
-                <ChevronDownIcon key={index} item={item} className="w-5 h-5 text-gray-500" />
-          ))}
+             {products.map((item,index)=>(
+                 <ProductCards key={index} item={item}/>
+              ))}
+                <ChevronDownIcon className="w-5 h-5 text-gray-500" />
               </MenuButton>
             </div>
 
@@ -181,6 +181,7 @@ const AllProduct = () => {
 };
 
 export default AllProduct;
+
 
 
 
