97d3ca8e47f3ef18f1d25672a63f8dcb6987181e
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-12 00:32:52 +0700

    Update AllProduct.jsx

--- src/pages/product/AllProduct.jsx
+++ src/pages/product/AllProduct.jsx
@@ -106,7 +106,7 @@ const handleCheck = (e) => {
           <Menu as="div" className="relative inline-block text-left">
             <div>
               <MenuButton className="inline-flex items-center gap-x-2 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow hover:bg-gray-100 ring-1 ring-gray-300 transition-all duration-200">
-                {/* <SearchCardAllProduct /> */}
+                <SearchCardAllProduct />
                 <ChevronDownIcon className="w-5 h-5 text-gray-500" />
               </MenuButton>
             </div>
@@ -201,6 +201,7 @@ const handleCheck = (e) => {
 };
 
 export default AllProduct;
+
 
 
 
