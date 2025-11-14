d83b72f168df8edfbf519d660fce8ccdfa0dedbd
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-14 23:00:15 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,6 +56,15 @@ export default {
         '0%': { opacity: 0, transform: 'translateY(20px)' },
         '100%': { opacity: 1, transform: 'translateY(0)' },
       },
+            keyframes:{
+          glow: {
+          '0%, 100%': { boxShadow: '0 0 8px #3b82f6, 0 0 12px #1e40af' },
+          '50%': { boxShadow: '0 0 12px #3b82f6, 0 0 16px #1e40af' },
+        }
+      },
+       animation: {
+        glow: 'glow 2s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
