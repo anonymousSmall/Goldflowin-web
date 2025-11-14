e557b2dd06eaef5007630e21390110d9cab5c752
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2025-11-15 00:04:25 +0700

    Update SidebarAdmin.jsx

--- src/components/admin/SidebarAdmin.jsx
+++ src/components/admin/SidebarAdmin.jsx
@@ -84,9 +84,10 @@ const SidebarAdmin = () => {
                 position: "top-right",
                 autoClose: 1200,
               });
-               setTimeout(() => {
-                navigate("/");   // Redirect ไปหน้า Home
-              }, 1200);          // ดีเลย์ให้ Toast แสดงก่อน
+              navigate("/");
+              //  setTimeout(() => {
+              //   navigate("/");   // Redirect ไปหน้า Home
+              // }, 1200);          // ดีเลย์ให้ Toast แสดงก่อน
             }}
             className="flex w-full items-center px-4 py-2 rounded-md text-base font-medium text-gray-600 hover:bg-red-100 hover:text-red-600 transition-all duration-200"
           >
@@ -108,5 +109,6 @@ const SidebarAdmin = () => {
 };
 
 export default SidebarAdmin;
+
 
 
