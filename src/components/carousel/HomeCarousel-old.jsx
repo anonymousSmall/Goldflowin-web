90eabae2d6eb6a25df78928d369217f0a246b21a
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-01-12 15:58:52 +0700

    Create HomeCarousel-old.jsx

---
+++ src/components/carousel/HomeCarousel-old.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import slide1 from "../../assets/image/photoslide/3.png";
+import slide2 from "../../assets/image/photoslide/4.png";
+import slide3 from "../../assets/image/photoslide/5.png";
+import slide4 from "../../assets/image/photoslide/6.png";
+import slide5 from "../../assets/image/photoslide/7.png";
+import slide6 from "../../assets/image/photoslide/8.png";
+import slide7 from "../../assets/image/photoslide/9.png";
+import slide8 from "../../assets/image/photoslide/10.png";
+import slide9 from "../../assets/image/photoslide/11.png";
+import slide10 from "../../assets/image/photoslide/12.png";
+import slide11 from "../../assets/image/photoslide/13.png";
+
+const HomeCarousel = () => {
+  return (
+    <Carousel
+      autoPlay={true}
+      showThumbs={false}
+      showArrows={false}
+      showStatus={false}
+      interval={2000}
+      infiniteLoop={true}
+    >
+      <div>
+        <img src={slide1} alt="" className="w-250" />
+      </div>
+      {/* <div>
+        <img src={slide2} alt="" />
+      </div>
+      <div>
+        <img src={slide3} alt="" />
+      </div>
+      <div>
+        <img src={slide4} alt="" />
+      </div>
+      <div>
+        <img src={slide5} alt="" />
+      </div>
+      <div>
+        <img src={slide6} alt="" />
+      </div>
+      <div>
+        <img src={slide7} alt="" />
+      </div>
+      <div>
+        <img src={slide8} alt="" />
+      </div>
+      <div>
+        <img src={slide9} alt="" />
+      </div>
+      <div>
+        <img src={slide10} alt="" />
+      </div>
+      <div>
+        <img src={slide11} alt="" />
+      </div> */}
+    </Carousel>
+  );
+};
+
+export default HomeCarousel;
