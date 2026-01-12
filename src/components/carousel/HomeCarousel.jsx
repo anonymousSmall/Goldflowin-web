09c548d0090f11ad8917907be4f456187db93ebc
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-01-12 13:18:30 +0700

    Update HomeCarousel.jsx

--- src/components/carousel/HomeCarousel.jsx
+++ src/components/carousel/HomeCarousel.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import slide1 from "../../assets/image/photoslide/2.png";
-import slide2 from "../../assets/image/photoslide/3.png";
-import slide3 from "../../assets/image/photoslide/4.png";
-import slide4 from "../../assets/image/photoslide/5.png";
-import slide5 from "../../assets/image/photoslide/6.png";
-import slide6 from "../../assets/image/photoslide/7.png";
-import slide7 from "../../assets/image/photoslide/8.png";
-import slide8 from "../../assets/image/photoslide/9.png";
-import slide9 from "../../assets/image/photoslide/10.png";
-import slide10 from "../../assets/image/photoslide/11.png";
-import slide11 from "../../assets/image/photoslide/12.png";
-import slide12 from "../../assets/image/photoslide/13.png";
+import slide1 from "../../assets/image/photoslide/3.png";
+import slide2 from "../../assets/image/photoslide/4.png";
+import slide3 from "../../assets/image/photoslide/5.png";
+import slide4 from "../../assets/image/photoslide/6.png";
+import slide5 from "../../assets/image/photoslide/7.png";
+import slide6 from "../../assets/image/photoslide/8.png";
+import slide7 from "../../assets/image/photoslide/9.png";
+import slide8 from "../../assets/image/photoslide/10.png";
+import slide9 from "../../assets/image/photoslide/11.png";
+import slide10 from "../../assets/image/photoslide/12.png";
+import slide11 from "../../assets/image/photoslide/13.png";
 
 const HomeCarousel = () => {
   return (
@@ -57,14 +56,12 @@ const HomeCarousel = () => {
       <div>
         <img src={slide11} alt="" />
       </div>
-      <div>
-        <img src={slide12} alt="" />
-      </div>
     </Carousel>
   );
 };
 
 export default HomeCarousel;
+
 
 
 
