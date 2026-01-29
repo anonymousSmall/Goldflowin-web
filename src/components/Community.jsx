1387efed218e2c5345f5d8b2d5f6fe00c5b7f99c
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-01-29 11:15:35 +0700

    Update Community.jsx

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -12,17 +12,17 @@ function Community() {
 
             <div className='grid grid-cols-1 gap-[100px] m-10 md:m-0 md:grid-cols-3'>
                 <div className='shadow-lg p-10 rounded-lg'>
-                    <img className='mx-auto' src={icon01} alt="" />
+                    <img className='mx-auto' src={photo1} alt="" />
                     <h3 className='text-[1.75rem] font-bold text-[#4D4D4D] my-3'>เครื่องมือวัด</h3>
                     <p>บริษัท โกลโฟล อินสตรูเมนท์ จำกัด เป็นตัวแทนจำหน่ายเครื่องมือวัดละเอียดทางด้านอุตสาหกรรม เช่น Video Measuring Machine(VMM), Profile Projector, CMM, ARM,, Contour, Roundness, Surftestm Stereo Microscope, และ Handness</p>
                 </div>
                 <div className='shadow-lg p-10 rounded-lg'>
-                    <img className='mx-auto' src={icon02} alt="" />
+                    <img className='mx-auto' src={photo2} alt="" />
                     <h3 className='text-[1.75rem] font-bold text-[#4D4D4D] my-3'>บริการสอบเทียบ</h3>
                     <p>บริษัท โกลโฟล อินสตรูเมนท์ จำกัด จำกัด มีการบริการสอบเทียบเครื่องมือวัดทางด้านอุตสาหกรรม และ เครื่องชั่งทั้งในและนอกสถานที่ด้วยมาตรฐานการรับรองห้องปฏิบัติการสอบเทียบ มอก. 17025-2548 ตามขอบข่าย</p>
                 </div>
                 <div className='shadow-lg p-10 rounded-lg'>
-                    <img className='mx-auto' src={icon03} alt="" />
+                    <img className='mx-auto' src={photo4} alt="" />
                     <h3 className='text-[1.75rem] font-bold text-[#4D4D4D] my-3'>บริการซ่อมเครื่องมือวัด/อุปกรณ์ต่างๆ</h3>
                     <p>ให้บริการซ่อมเครื่องมือวัดต่างๆ -PROFILE PROJECTOR -VIDEO MEASURING -เครื่องชั่งน้ำหนัก -Vernier Caliper,Micrometer และอื่นๆ</p>
                 </div>
@@ -32,6 +32,7 @@ function Community() {
 }
 
 export default Community;
+
 
 
 
