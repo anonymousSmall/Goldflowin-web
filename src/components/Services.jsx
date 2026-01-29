d49c466935802635c295590fb06370031ba839cb
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-01-29 16:06:35 +0700

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,12 +3,12 @@ import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
 
 function Services(){
 return (
-  <div className='bg-[#F5F7FA] py-10 flex justify-center text-center'>
-    <div>
-      <h3 className='text-[#1E3A8A] text-[2.25rem] font-semibold leading-[1]'>ผลงานและบริการของเรา</h3>
-      <a href="" className='line-flex justity-center items-center py-3 px-8 mt-5 bg-[#4CAF4F] text-while rounded-md'>ดูผลงานทั้งหมด <HiOutlineArrowNarrowRight className='inline' /> </a>
+    <div className='bg-[#F5F7FA] py-10 flex justify-center text-center'>
+      <div>
+        <h3 className='text-[#1E3A8A] text-[2.25rem] font-semibold leading-[1]'>ผลงานและบริการของเรา</h3>
+        <a href="" className='inline-flex justify-center items-center py-3 px-8 mt-5 bg-[#4CAF4F] text-white rounded-md'>ดูผลงานทั้งหมด <HiOutlineArrowNarrowRight className='inline' /></a>
+      </div>
     </div>
-  </div>
   )
 }
 
