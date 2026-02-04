cfb7f695e9d22fe10febd3dce9fa40f4f082aa55
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-02-04 16:38:16 +0700

    Update Client.jsx

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -16,37 +16,6 @@ import clientImgLogo14 from "../assets/image/img-logo3.png";
 import HomeCarouselBrand from "./carousel/HomeCarouselBrand";
 import BrandSlider from "./BrandSlider";
 
-import accretech from "../assets/image/logo/accretech.png";
-import accud from "../assets/image/logo/accud.png";
-import aikoh from "../assets/image/logo/aikoh.png";
-import carmar from "../assets/image/logo/carmar.png";
-import carton from "../assets/image/logo/carton.png";
-import chuer from "../assets/image/logo/chuer.png";
-import digicon from "../assets/image/logo/digicon.png";
-import eisen from "../assets/image/logo/eisen.png";
-import fluk from "../assets/image/logo/fluk.png";
-import gin from "../assets/image/logo/gin.png";
-
-
-import hexagon from "../assets/image/logo/hexagon.png";
-import imada from "../assets/image/logo/imada.png";
-import insize from "../assets/image/logo/insize.png";
-import jadever from "../assets/image/logo/jadever.png";
-import mahr from "../assets/image/logo/mahr.png";
-import mitutoyo from "../assets/image/logo/mitutoyo.png";
-import ojiyas from "../assets/image/logo/ojiyas.png";
-import peacock from "../assets/image/logo/peacock.png";
-import shahe from "../assets/image/logo/shahe.png";
-import shimadzu from "../assets/image/logo/shimadzu.png";
-
-
-import zepper from "../assets/image/logo/zepper.png";
-import tboss from "../assets/image/logo/t-boss.png";
-import tanita from "../assets/image/logo/tanita.png";
-import vertex from "../assets/image/logo/vertex.png";
-import morewright from "../assets/image/logo/more&wright.png";
-import mikrosize from "../assets/image/logo/mokrosize.png";
-
 const clientsImgLogo = [
   accretech,
   accud,
@@ -62,13 +31,28 @@ const clientsImgLogo = [
 
 function Client() {
   return (
-    <div>
-    <BrandSlider/>
+<div className="container mx-auto max-w-[1320px] py-10 text-center">
+      <h2 className="text-[2.25rem] font-semibold text-blue-900">
+        แบรนด์ที่จัดจำหน่าย
+      </h2>
+      <p className="text-[#717171] text-[1.4rem] mb-5 inline-block border-b-2 border-[#3936fa]">
+        Our Service
+      </p>
+      <ul className="flex flex-col items-center my-10 md:flex-row md:justify-between">
+        {/* {clientsTmgLogo.map((client,index)=>(
+            <li key={index}>
+                <img src={client} alt="" />
+            </li>
+        ))} */}
+        {/* <HomeCarouselBrand /> */}
+        <BrandSlider/>
+      </ul>
     </div>
   );
 }
 
 export default Client;
+
 
 
 
