32eb5313b93854ea93720de9d28faf284d6f6788
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-02-04 16:46:40 +0700

    Update Client.jsx

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -40,9 +40,6 @@ function Client() {
       <h2 className="text-[2.25rem] font-semibold text-blue-900">
         แบรนด์ที่จัดจำหน่าย
       </h2>
-      <p className="text-[#717171] text-[1.4rem] mb-5 inline-block border-b-2 border-[#3936fa]">
-        Our Service
-      </p>
       <ul className="flex flex-col items-center my-10 md:flex-row md:justify-between">
         {/* {clientsTmgLogo.map((client,index)=>(
             <li key={index}>
@@ -57,4 +54,5 @@ function Client() {
 }
 
 export default Client;
+
 
