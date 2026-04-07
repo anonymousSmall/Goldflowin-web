77eb4a5bbab8fb0574b0da5d5e90e6280ad5744c
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-07 14:05:15 +0700

    Refactor App component and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 //rafce
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // import AOS from "aos";
 // import "aos/dist/aos.css";
+import Footers from './components/Footers'
 import MainFooter from "./components/MainFooter";
 import Header from "./components/Header";
 
@@ -18,14 +19,16 @@ const App = () => {
   //     delay: 100,
   //   });
   // });
+  const [count, setCount] = useState(0)
   return (
     <>
-      <ToastContainer />
+      {/* <ToastContainer /> */}
       {/* <div className="bg-red-400"> */}
       <AppRoutes />
       {/* <Header/> */}
       {/* </div> */}
-        <MainFooter />
+      {/* <MainFooter /> */}
+      <Footers />
     </>
   );
 };
